c6067c071691fb8ad5260d59beb528e24cc3a981
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-05-09 09:59:33 +0200

    created components Header and Footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+<div class="row">
+    <p class="copyright">© Groupomania 2022</p>
+  </div>
+</template>
+
+<style scoped>
+.copyright {
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<script >
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <div class="container-sm d-flex my-4 justify-content-center">
+    <section class="row">
+      <div class="col">
+        <!-- Logo Groupomania -->
+        <img src="../assets/icon.svg" alt="logo" />
+        <h1> Groupomania</h1>
+      </div>
+    </section>
+ </div>
+</template>
