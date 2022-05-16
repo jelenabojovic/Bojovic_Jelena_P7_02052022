0f4150a81b0da0dd3c364aa398e9cca9e7784e17
Jelena Bojovic <jelenabojovic981@gmail.com>
2022-05-16 13:45:44 +0200

    creation and connection to database

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+// Imports
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+// Express
+const app = express();
+
+// Routes
+//const userRoutes = require("./routes/user");
+//const postRoutes = require("./routes/post");
+//const commentRoutes = require("./routes/comment");
+
+// Connexion BDD
+const sequelize = new Sequelize("groupomania", "root", "**JB!fr!2022", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  sequelize.authenticate();
+  console.log("Connecté à la base de données MySql");
+} catch (error) {
+  console.error("Impossible de se connecter à la base de données:", error);
+}
+
+// erreur de CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use(express.json());
+
+// Middleware
+//app.use("/images", express.static(path.join(__dirname, "images")));
+//app.use("/api/auth", userRoutes);
+//app.use("/api/post", postRoutes);
+//app.use("/api/comment", commentRoutes);
+
+module.exports = app;
---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+    firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false },
+      avatar: { type: DataTypes.STRING, allowNull: true },
+      service: { type: DataTypes.STRING, allowNull: false },
+      isAdmin: { type: DataTypes.BOOLEAN, defaultValue: 0 },
+    }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
